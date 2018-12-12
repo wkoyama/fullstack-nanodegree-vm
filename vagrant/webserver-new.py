333db25f31087f991f9ad68d5777c772d9cebb25
Wesley Kenji <wkenji.oyama@gmail.com>
2018-12-12 02:12:04 -0200

    objetivo 3 mais ajustes de multipart

--- vagrant/webserver-new.py
+++ vagrant/webserver-new.py
@@ -47,6 +47,7 @@ import http.server
 import threading
 from socketserver import ThreadingMixIn
 from urllib.parse import unquote, parse_qs
+import cgi
 
 # import CRUD Operations from Lesson 1
 from database_setup import Base, Restaurant, MenuItem
@@ -71,9 +72,27 @@ class WebServerHandler(http.server.BaseHTTPRequestHandler):
         # Strip off the / and we have either empty string or a name.
         name = unquote(self.path[1:])
 
-        if name == "restaurants":
+        # Objective 3 Step 2 - Create /restarants/new page
+        if self.path.endswith("/restaurants/new"):
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            output = ""
+            output += "<html><body>"
+            output += "<h1>Make a New Restaurant</h1>"
+            output += "<form method = 'POST' enctype='multipart/form-data' action = '/restaurants/new'>"
+            output += "<input name = 'newRestaurantName' type = 'text' placeholder = 'New Restaurant Name' > "
+            output += "<input type='submit' value='Create'>"
+            output += "</form></body></html>"
+            self.wfile.write(output.encode())
+            return
+
+        if self.path.endswith("/restaurants"):
             restaurants = session.query(Restaurant).all()
             output = ""
+            # Objective 3 Step 1 - Create a Link to create a new menu item
+            output += "<a href = '/restaurants/new' > Make a New Restaurant Here </a></br></br>"
+
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
@@ -81,6 +100,11 @@ class WebServerHandler(http.server.BaseHTTPRequestHandler):
             output += "<html><body>"
             for restaurant in restaurants:
                 output += restaurant.name
+                output += "</br>"
+                # Objective 2 -- Add Edit and Delete Links
+                output += "<a href ='#' >Edit </a> "
+                output += "</br>"
+                output += "<a href =' #'> Delete </a>"
                 output += "</br></br></br>"
 
             output += "</body></html>"
@@ -93,6 +117,32 @@ class WebServerHandler(http.server.BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write("I don't know '{}'.".format(name).encode())
 
+    # Objective 3 Step 3- Make POST method
+    def do_POST(self):
+        try:
+            if self.path.endswith("/restaurants/new"):
+                ctype, pdict = cgi.parse_header(
+                    self.headers['content-type'])
+
+                pdict['boundary'] = bytes(pdict['boundary'], "utf-8")
+                pdict['CONTENT-LENGTH'] = self.headers['content-length']
+
+                if ctype == 'multipart/form-data':
+                    fields = cgi.parse_multipart(self.rfile, pdict)
+                    messagecontent = fields.get('newRestaurantName')
+
+                    # Create new Restaurant Object
+                    newRestaurant = Restaurant(name=messagecontent[0])
+                    session.add(newRestaurant)
+                    session.commit()
+
+                    self.send_response(301)
+                    self.send_header('Content-type', 'text/html')
+                    self.send_header('Location', '/restaurants')
+                    self.end_headers()
+
+        except:
+            raise
 
 if __name__ == '__main__':
     try:
