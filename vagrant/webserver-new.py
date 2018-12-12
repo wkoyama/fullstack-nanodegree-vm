d430ec4464885cda22b1037f3ec34b74a0cf639d
Wesley Kenji <wkenji.oyama@gmail.com>
2018-12-12 02:32:07 -0200

    edicao e remocao de restaurantes

--- vagrant/webserver-new.py
+++ vagrant/webserver-new.py
@@ -71,46 +71,87 @@ class WebServerHandler(http.server.BaseHTTPRequestHandler):
         # A GET request will either be for / (the root path) or for /some-name.
         # Strip off the / and we have either empty string or a name.
         name = unquote(self.path[1:])
+        try:
+            # Objective 3 Step 2 - Create /restarants/new page
+            if self.path.endswith("/restaurants/new"):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                output = ""
+                output += "<html><body>"
+                output += "<h1>Make a New Restaurant</h1>"
+                output += "<form method = 'POST' enctype='multipart/form-data' action = '/restaurants/new'>"
+                output += "<input name = 'newRestaurantName' type = 'text' placeholder = 'New Restaurant Name' > "
+                output += "<input type='submit' value='Create'>"
+                output += "</form></body></html>"
+                self.wfile.write(output.encode())
+                return
 
-        # Objective 3 Step 2 - Create /restarants/new page
-        if self.path.endswith("/restaurants/new"):
-            self.send_response(200)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            output = ""
-            output += "<html><body>"
-            output += "<h1>Make a New Restaurant</h1>"
-            output += "<form method = 'POST' enctype='multipart/form-data' action = '/restaurants/new'>"
-            output += "<input name = 'newRestaurantName' type = 'text' placeholder = 'New Restaurant Name' > "
-            output += "<input type='submit' value='Create'>"
-            output += "</form></body></html>"
-            self.wfile.write(output.encode())
-            return
+            if self.path.endswith("/delete"):
+                restaurantIDPath = self.path.split("/")[2]
 
-        if self.path.endswith("/restaurants"):
-            restaurants = session.query(Restaurant).all()
-            output = ""
-            # Objective 3 Step 1 - Create a Link to create a new menu item
-            output += "<a href = '/restaurants/new' > Make a New Restaurant Here </a></br></br>"
+                myRestaurantQuery = session.query(Restaurant).filter_by(
+                    id=restaurantIDPath).one()
+                if myRestaurantQuery:
+                    self.send_response(200)
+                    self.send_header('Content-type', 'text/html')
+                    self.end_headers()
+                    output = ""
+                    output += "<html><body>"
+                    output += "<h1>Are you sure you want to delete %s?" % myRestaurantQuery.name
+                    output += "<form method='POST' enctype = 'multipart/form-data' action = '/restaurants/%s/delete'>" % restaurantIDPath
+                    output += "<input type = 'submit' value = 'Delete'>"
+                    output += "</form>"
+                    output += "</body></html>"
+                    self.wfile.write(output.encode())
 
-            self.send_response(200)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
+            if self.path.endswith("/edit"):
+                restaurantIDPath = self.path.split("/")[2]
+                myRestaurantQuery = session.query(Restaurant).filter_by(
+                    id=restaurantIDPath).one()
+                if myRestaurantQuery:
+                    self.send_response(200)
+                    self.send_header('Content-type', 'text/html')
+                    self.end_headers()
+                    output = "<html><body>"
+                    output += "<h1>"
+                    output += myRestaurantQuery.name
+                    output += "</h1>"
+                    output += "<form method='POST' enctype='multipart/form-data' action = '/restaurants/%s/edit' >" % restaurantIDPath
+                    output += "<input name = 'newRestaurantName' type='text' placeholder = '%s' >" % myRestaurantQuery.name
+                    output += "<input type = 'submit' value = 'Rename'>"
+                    output += "</form>"
+                    output += "</body></html>"
 
-            output += "<html><body>"
-            for restaurant in restaurants:
-                output += restaurant.name
-                output += "</br>"
-                # Objective 2 -- Add Edit and Delete Links
-                output += "<a href ='#' >Edit </a> "
-                output += "</br>"
-                output += "<a href =' #'> Delete </a>"
-                output += "</br></br></br>"
+                    self.wfile.write(output.encode())
 
-            output += "</body></html>"
-            self.wfile.write(output.encode())
-            return
-        else:
+            if self.path.endswith("/restaurants"):
+                restaurants = session.query(Restaurant).all()
+                output = ""
+                # Objective 3 Step 1 - Create a Link to create a new menu item
+                output += "<a href = '/restaurants/new' > Make a New Restaurant Here </a></br></br>"
+
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+
+                output += "<html><body>"
+                for restaurant in restaurants:
+                    output += restaurant.name
+                    output += "</br>"
+                    # Objective 2 -- Add Edit and Delete Links
+                    # Objective 4 -- Replace Edit href
+
+                    output += "<a href ='/restaurants/%s/edit' >Edit </a> " % restaurant.id
+                    output += "</br>"
+                    # Objective 5 -- Replace Delete href
+                    output += "<a href ='/restaurants/%s/delete'> Delete </a>" % restaurant.id
+                    output += "</br></br></br>"
+
+                output += "</body></html>"
+                self.wfile.write(output.encode())
+                return
+        except IOError:
             # We don't know that name! Send a 404 error.
             self.send_response(404)
             self.send_header('Content-type', 'text/plain; charset=utf-8')
@@ -120,6 +161,42 @@ class WebServerHandler(http.server.BaseHTTPRequestHandler):
     # Objective 3 Step 3- Make POST method
     def do_POST(self):
         try:
+            if self.path.endswith("/delete"):
+                restaurantIDPath = self.path.split("/")[2]
+                
+                myRestaurantQuery = session.query(Restaurant).filter_by(
+                    id=restaurantIDPath).one()
+                if myRestaurantQuery:
+                    session.delete(myRestaurantQuery)
+                    session.commit()
+                    self.send_response(301)
+                    self.send_header('Content-type', 'text/html')
+                    self.send_header('Location', '/restaurants')
+                    self.end_headers()
+
+            if self.path.endswith("/edit"):
+                ctype, pdict = cgi.parse_header(
+                    self.headers['content-type'])
+                
+                pdict['boundary'] = bytes(pdict['boundary'], "utf-8")
+                pdict['CONTENT-LENGTH'] = self.headers['content-length']
+
+                if ctype == 'multipart/form-data':
+                    fields = cgi.parse_multipart(self.rfile, pdict)
+                    messagecontent = fields.get('newRestaurantName')
+                    restaurantIDPath = self.path.split("/")[2]
+
+                    myRestaurantQuery = session.query(Restaurant).filter_by(
+                        id=restaurantIDPath).one()
+                    if myRestaurantQuery != []:
+                        myRestaurantQuery.name = messagecontent[0]
+                        session.add(myRestaurantQuery)
+                        session.commit()
+                        self.send_response(301)
+                        self.send_header('Content-type', 'text/html')
+                        self.send_header('Location', '/restaurants')
+                        self.end_headers()
+
             if self.path.endswith("/restaurants/new"):
                 ctype, pdict = cgi.parse_header(
                     self.headers['content-type'])
