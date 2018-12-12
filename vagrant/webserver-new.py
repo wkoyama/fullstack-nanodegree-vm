60eb665592ff2ce1c3c5e433a92f967076ded0a5
Wesley Kenji <wkenji.oyama@gmail.com>
2018-12-12 01:02:27 -0200

    servidor funcional com multithread

---
+++ vagrant/webserver-new.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+#
+# A *bookmark server* or URI shortener that maintains a mapping (dictionary)
+# between short names and long URIs, checking that each new URI added to the
+# mapping actually works (i.e. returns a 200 OK).
+#
+# This server is intended to serve three kinds of requests:
+#
+#   * A GET request to the / (root) path.  The server returns a form allowing
+#     the user to submit a new name/URI pairing.  The form also includes a
+#     listing of all the known pairings.
+#   * A POST request containing "longuri" and "shortname" fields.  The server
+#     checks that the URI is valid (by requesting it), and if so, stores the
+#     mapping from shortname to longuri in its dictionary.  The server then
+#     redirects back to the root path.
+#   * A GET request whose path contains a short name.  The server looks up
+#     that short name in its dictionary and redirects to the corresponding
+#     long URI.
+#
+# Your job in this exercise is to finish the server code.
+#
+# Here are the steps you need to complete:
+#
+# 1. Write the CheckURI function, which takes a URI and returns True if a
+#    request to that URI returns a 200 OK, and False otherwise.
+#
+# 2. Write the code inside do_GET that sends a 303 redirect to a known name.
+#
+# 3. Write the code inside do_POST that sends a 400 error if the form fields
+#    are missing.
+#
+# 4. Write the code inside do_POST that sends a 303 redirect to the form
+#    after saving a newly submitted URI.
+#
+# 5. Write the code inside do_POST that sends a 404 error if a URI is not
+#    successfully checked (i.e. if CheckURI returns false).
+#
+# In each step, you'll need to delete a line of code that raises the
+# NotImplementedError exception.  These are there as placeholders in the
+# starter code.
+#
+# After writing each step, restart the server and run test.py to test it.
+
+import os
+import http.server
+# import requests
+import threading
+from socketserver import ThreadingMixIn
+from urllib.parse import unquote, parse_qs
+
+# import CRUD Operations from Lesson 1
+from database_setup import Base, Restaurant, MenuItem
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+# Create session and connect to DB
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+form = '''
+'''
+
+class ThreadHTTPServer(ThreadingMixIn, http.server.HTTPServer):
+    "This is an HTTPServer that supports thread-based concurrency."
+
+class WebServerHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        # A GET request will either be for / (the root path) or for /some-name.
+        # Strip off the / and we have either empty string or a name.
+        name = unquote(self.path[1:])
+
+        if name == "restaurants":
+            restaurants = session.query(Restaurant).all()
+            output = ""
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+
+            output += "<html><body>"
+            for restaurant in restaurants:
+                output += restaurant.name
+                output += "</br></br></br>"
+
+            output += "</body></html>"
+            self.wfile.write(output.encode())
+            return
+        else:
+            # We don't know that name! Send a 404 error.
+            self.send_response(404)
+            self.send_header('Content-type', 'text/plain; charset=utf-8')
+            self.end_headers()
+            self.wfile.write("I don't know '{}'.".format(name).encode())
+
+
+if __name__ == '__main__':
+    try:
+        port = int(os.environ.get('PORT', 8000))   # Utilize a variável PORT caso ela exista.
+        server_address = ('', port)
+        httpd = ThreadHTTPServer(server_address, WebServerHandler)
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        httpd.shutdown()
